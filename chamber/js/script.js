971e79eecd5b8a56d7faa01ed15d09eb4fd21d85
ajbxyzcool <bla10037@byui.edu>
2022-02-17 15:31:30 -0600

    Further work on Week 4

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -1,8 +1,10 @@
-function copyright(){
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+const fulldateUK = new Intl.DateTimeFormat("en-UK", { dateStyle: "full" }).format(now);
 
-    document.querySelector("#copyright").innerHTML = new Date().getFullYear();
-    let x = document.lastModified;
-    document.getElementById("modified").innerHTML = `Last Updated: ${x}`;
+document.querySelector("#currentdate").innerHTML = `${fulldate}`;
+document.querySelector("aside").innerHTML = `${fulldateUK}`;
 
-}
-
+document.querySelector("#copyright").innerHTML = `${now.getFullYear()}`;
+let x = document.lastModified;
+document.getElementById("modified").innerHTML = `Last Updated: ${x}`;
