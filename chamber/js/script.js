92c491c8cec5c32148399b3ad080e57aac906fdf
ajbxyzcool <bla10037@byui.edu>
2022-03-22 19:36:20 -0500

    Finished W04 assignment

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -1,10 +1,17 @@
 const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
-const fulldateUK = new Intl.DateTimeFormat("en-UK", { dateStyle: "full" }).format(now);
 
 document.querySelector("#currentdate").innerHTML = `${fulldate}`;
-document.querySelector("aside").innerHTML = `${fulldateUK}`;
 
 document.querySelector("#copyright").innerHTML = `${now.getFullYear()}`;
 let x = document.lastModified;
 document.getElementById("modified").innerHTML = `Last Updated: ${x}`;
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
